0d6fca9e58b49a36ace43ae80c5c7e30cf47df48
Ömer Aşıkoğlu <faruk.asikoglu@gmail.com>
2024-04-18 00:41:17 +0300

    LevelLoader.cs

---
+++ Assets/Game/Dev/Network/GameManager.cs
@@ -0,0 +1,17 @@
+﻿namespace SystemSpace{
+
+  public class GameManager{
+
+    readonly LevelLoader levelLoader = new();
+
+    public GameManager(){
+      UnityEngine.Debug.Log($"{this} Injected");
+    }
+
+    bool CheckIsLevelLoaded(){
+      return levelLoader.IsLevelCreated;
+    }
+
+  }
+
+}
---
+++ Assets/Game/Dev/Network/LevelLoader.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SystemSpace{
+
+  public class LevelLoader{
+
+    AsyncOperation currentLevelLoadOperation;
+
+    string currentLevelSceneName = string.Empty;
+
+    public bool IsLevelCreated{get; private set;}
+
+    public LevelLoader(int levelNumber = 1){
+      LoadLevel(levelNumber);
+    }
+
+    public void LoadLevel(int levelNumber){
+      if (IsLevelCreated) return;
+      if (currentLevelLoadOperation != null) return;
+
+      string levelSceneName = "Level" + levelNumber.ToString("D2");
+
+      currentLevelLoadOperation = SceneManager.LoadSceneAsync(levelSceneName, LoadSceneMode.Additive);
+
+      currentLevelLoadOperation.completed += op => {
+        currentLevelSceneName = levelSceneName;
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName(levelSceneName));
+        IsLevelCreated            = true;
+        currentLevelLoadOperation = null;
+      };
+    }
+
+    public void UnloadCurrentLevel(){
+      if (!IsLevelCreated) return;
+
+      SceneManager.UnloadSceneAsync(currentLevelSceneName);
+      IsLevelCreated        = false;
+      currentLevelSceneName = string.Empty;
+    }
+
+  }
+
+}
